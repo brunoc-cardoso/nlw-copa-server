06151385596d88901b5589fa238cde44a49f0f41
brunoc-cardoso <brunocpessoal@gmail.com>
2022-11-03 17:53:39 -0300

    add fastify cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import { prisma } from '@/services/prisma';
+import cors from '@fastify/cors';
 import Fastify from 'fastify';
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
+  });
+
+  await fastify.register(cors, {
+    origin: true,
   });
 
   fastify.get('/pools/count', async () => {
@@ -14,7 +19,7 @@ async function bootstrap() {
     };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: '0.0.0.0.' });
 }
 
 bootstrap();
